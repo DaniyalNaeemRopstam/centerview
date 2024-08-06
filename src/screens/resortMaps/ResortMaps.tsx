adabec76c39c2cf5a4e85f4330b5fc7c8306ade5
MalikSaadTanveer <malik.saad@ropstam.com>
2024-08-06 14:23:32 +0500

    zoom image

--- src/screens/resortMaps/ResortMaps.tsx
+++ src/screens/resortMaps/ResortMaps.tsx
@@ -1,18 +1,42 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, ImageComponent, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import WebView from 'react-native-webview'
-import Maps from '../../assets/Maps1.jpg'
+import Maps from '../../assets/Maps2.png'
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
+import { heightPercentageToDP, widthPercentageToDP } from 'react-native-responsive-screen';
 
 const ResortMaps = () => {
     return (
+        <ReactNativeZoomableView
+            maxZoom={5}
+            minZoom={1}
+            zoomStep={0.5}
+            initialZoom={1}
+            bindToBorders={true}
+            style={styles.imageContainer}
+            contentHeight={heightPercentageToDP(50)}
+        >
             <Image
                 source={Maps}
-                style={{ flex: 1, width:'100%' }}
-                resizeMode='stretch'
+                style={styles.img}
+                resizeMode='contain'
             />
+        </ReactNativeZoomableView>
     )
 }
 
 export default ResortMaps
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imageContainer:
+    {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:widthPercentageToDP(100),
+    },
+    img: {
+        width: '100%',
+        height:'100%',
+    }
+})
